8109bbed13420a7c0e445915d24369922b5059ed
ranwawa <ranwawa.ran@lalamove.com>
2022-02-23 10:48:20 +0800

    feat: 初始化

---
+++ packages/ranwawa-test-a/packages/ranwawa-test-a/rules/border-radius.js
@@ -0,0 +1,24 @@
+"use strict";
+exports.__esModule = true;
+var stylelint = require("stylelint");
+var index_1 = require("../utils/index");
+var ruleMessages = stylelint.utils.ruleMessages;
+var RULE_PROPERTY = 'border-radius';
+var VARIABLE_REG = /^bdrs-[a-z]+$/;
+var DEFAULT_OPTION = {
+    "bdrs-round": "50%",
+    "bdrs-xl": "16",
+    "bdrs-lg": "12",
+    "bdrs-md": "8",
+    "bdrs-sm": "4",
+    "bdrs-none": "0"
+};
+var ruleName = "ranwawa/".concat(RULE_PROPERTY, "-allowed-list");
+module.exports = (0, index_1.createAllowedListPluginFactory)({
+    ruleName: ruleName,
+    variableReg: VARIABLE_REG,
+    defaultOption: DEFAULT_OPTION,
+    ruleProperty: RULE_PROPERTY
+});
+module.exports.ruleName = ruleName;
+module.exports.messages = ruleMessages(ruleName, {});
---
+++ packages/ranwawa-test-a/packages/ranwawa-test-a/rules/index.js
@@ -0,0 +1,11 @@
+var _importLazy = require('import-lazy');
+var importLazy = _importLazy(require);
+module.exports = [
+    importLazy('./border-color'),
+    importLazy('./border-radius'),
+    importLazy('./border'),
+    importLazy('./box-shadow'),
+    importLazy('./color'),
+    importLazy('./font-size'),
+    importLazy('./z-index'),
+];
