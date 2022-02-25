6cfb90e7e42b7a41a69155771840359091bfd89e
ranwawa <ranwawa.ran@lalamove.com>
2022-02-25 11:03:34 +0800

    feat(root): 🎸 增加对stylelint-config的支持

--- changelog.config.js
+++ changelog.config.js
@@ -1,10 +1,13 @@
+const config = require('@ranwawa/git-cz-config');
+
 module.exports = {
-  ...require('@ranwawa/git-cz-config'),
+  ...config,
   scopes: [
     'root',
     '@ranwawa/commitlint-plugin',
     '@ranwawa/eslint-config-react',
     '@ranwawa/prettier-config',
+    '@ranwawa/stylelint-config-scss',
     '@ranwawa/stylelint-plugin-ui-convention',
   ],
 };
