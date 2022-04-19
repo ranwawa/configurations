cc2e1a0b858a2f76a26820d5c07da8d8bf14272a
ranwawa <274544338@qq.co>
2022-04-19 11:49:42 +0800

    test2

--- packages/branchlint/bin/index.mjs
+++ packages/branchlint/bin/index.mjs
@@ -1,74 +1,8 @@
 #!/usr/bin/env zx
 /* eslint-disable import/extensions */
 import { $, chalk, quiet } from 'zx';
-import _ from 'lodash';
-
-class Linter {
-  config = [];
-
-  branchName = '';
-
-  regStr = '';
-
-  constructor(branchName, config) {
-    this.branchName = branchName;
-    this.config = config;
-  }
-
-  lint() {
-    const newConfig = this.config.map((item) => {
-      const { pattern } = item;
-      const newItem = { ...item };
-
-      if (_.isArray(pattern)) {
-        newItem.regStr = `(${pattern.join('|')})`;
-        newItem.validateFunc = this.arry;
-      } else if (_.isString(pattern)) {
-        newItem.regStr = `(${pattern})`;
-        newItem.validateFunc = this.primitive;
-      } else if (_.isRegExp(pattern)) {
-        const str = pattern.toString();
-        newItem.regStr = `(${str.substring(1, str.length - 1)})`;
-        newItem.validateFunc = this.reg;
-      }
-
-      return newItem;
-    });
-    this.regStr = newConfig.map((ele) => ele.regStr).join('');
-    const reg = new RegExp(this.regStr);
-    return this.branchName.match(reg);
-  }
-}
-
-class Config {
-  defaultConfig = [
-    {
-      type: 'content',
-      pattern: ['feat', 'fix'],
-      message: '分支名只能以feat和fix开头',
-    },
-    {
-      type: 'separator',
-      pattern: '/',
-      message: '分隔符只能是/',
-    },
-    {
-      type: 'content',
-      pattern: /[a-zA-Z0-9]+/,
-      message: '名称只能是英文',
-    },
-    {
-      type: 'separator',
-      pattern: '/',
-      message: '分隔符只能是/',
-    },
-    {
-      type: 'content',
-      pattern: /[a-zA-Z0-9_]+/,
-      message: '内容只能是英文加下划线',
-    },
-  ];
-}
+import Linter from '../src/linter.js';
+import Config from '../src/config.js';
 
 async function init() {
   // 1. 获取分支名
