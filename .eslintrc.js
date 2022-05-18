24eb6481486d6003f8e8a09c552082675843d878
ranwawa <macbookpro@macbooks-MacBook-Pro.local>
2022-05-18 17:15:57 +0800

    build(root): 🎡 stylelint插件不用检测import require混用的情况

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ['@ranwawa/eslint-config-react'],
+  rules: {
+    'import/no-import-module-exports': [
+      'error',
+      {
+        exceptions: ['**/stylelint-plugin-ui-convention/**/*.ts'],
+      },
+    ],
+  },
 };
